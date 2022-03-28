4062071518862ed817edc54dc23e8a65f893acd6
Sravani0410 <sandhyapyla.8@gmail.com>
2022-03-28 11:24:27 +0530

    todo controller update

--- c4 evalution/src/controllers/todo.controllers.js
+++ c4 evalution/src/controllers/todo.controllers.js
@@ -6,8 +6,8 @@ router.post("",authenticate,async(req,res)=>{
 req.body.user_id=req.user._id;
     
     try{
-        const product=await Todo.create(req.body)
-        return res.status(200).send(product)
+        const todo=await Todo.create(req.body)
+        return res.status(200).send(todo)
     }
   catch(err){
       return res.status(400).send({message:err.message})
@@ -15,8 +15,35 @@ req.body.user_id=req.user._id;
 })
 router.get("",async(req,res)=>{
     try{
-        const product=await Todo.find()
-        return res.status(200).send(product)
+        const todo=await Todo.find()
+        return res.status(200).send(todo)
+    }
+    catch(err){
+        return res.status(400).send({message:err.message})
+    }
+})
+router.post("",async(req,res)=>{
+    try{
+        const todo=await Todo.create(req.body)
+        return res.status(200).send(todo)
+    }
+    catch(err){
+        return res.status(400).send({message:err.message})
+    }
+})
+router.patch("/users/:/id",async(req,res)=>{
+    try{
+        const todo=await Todo.findByIdAndUpdate(req.params.id,req,body,{new:true})
+        return res.status(200).send(todo)
+    }
+    catch(err){
+        return res.status(400).send({message:err.message})
+    }
+})
+router.delete("/users/:/id",async(req,res)=>{
+    try{
+        const todo=await Todo.findByIdAndDelete(req.params.id).lean().exec()
+        return res.status(200).send(todo)
     }
     catch(err){
         return res.status(400).send({message:err.message})
